31cfe9279cbd901c5a82b6248d570563f322444b
Bruno <bruno_moeller@proton.me>
2022-11-20 20:56:49 -0300

    added product styles and props not completed

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../Product';
 import { HomeContainer, BannerImage, Container, ProductsRow } from './styles';
 
+interface IProduct {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
+
 export default function Home() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+  .then((json) => {
+      console.log(json)
+      let slicedProducts: IProduct[] = [];
+      let count = 0
+      json.map((key: IProduct, index: number): void => {
+        if( index % 4 === 0 && index !== 0){
+          let rowOfProducts = json.slice(count, index)
+          slicedProducts.push(rowOfProducts)
+          count += 4;
+          }
+          // eslint-disable-next-line array-callback-return
+          return;
+        }
+      )
+
+      setProducts(slicedProducts)
+  });
+  }, [])
+
+  console.log(products)
+
   return (
     <Container>
       <HomeContainer>
@@ -11,10 +49,9 @@ export default function Home() {
           alt="ecommerce"
         />
         <ProductsRow>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+        {
+        
+        }
         </ProductsRow>
         <ProductsRow>
           <Product />
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -17,11 +17,16 @@ export const ProductContainer = styled.div`
   min-width: 8em;
   background-color: white;
   z-index: 1;
+  font-family: 'open sans', sans-serif;
 `;
 
 export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 8em;
   margin-bottom: 1em;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.p``;
@@ -34,7 +39,9 @@ export const Rating = styled.div`
   display: flex;
 `;
 
-export const Star = styled(StarIcon)``;
+export const Star = styled(StarIcon)`
+  color: yellow;
+`;
 
 export const Image = styled.img`
   max-height: 13em;
@@ -43,4 +50,12 @@ export const Image = styled.img`
   margin: .5em 0 0 .8em;
 `;
 
-export const AddToCart = styled.button``;
+export const AddToCart = styled.button`
+  font-family: 'open sans', sans-serif;
+  font-weight: 600;
+  margin-top: 1em;
+  background-color: #ff9f00;
+  border: 0;
+  padding: .5em 2em;
+  border-radius: 1em;
+`;
